0b3c36cd584488270f7c699af24ad8195850dee6
U-BSD\412709 <412709@SHS-9YBF6X3.beaverton.k12.or.us>
2023-10-11 08:23:39 -0700

    working program

--- swapFirstTwo.cpp
+++ swapFirstTwo.cpp
@@ -12,26 +12,31 @@ using namespace std;
 
 int main () {
   vector<int> spaceIndexes;
+  // Input
   char input[80];
   cin.get(input, 81);
   cin.get();
+  // Find spaces and add to vector
   for (int i = 0; i < strlen(input) - 1; i++) {
     if (input[i] == ' ' && input[i+2] != ' ') {
       spaceIndexes.push_back(i);
     }
   }
+  // Swap first two letters
   for (vector<int>::iterator iter = spaceIndexes.begin(); iter != (spaceIndexes.end() - 1); iter++) {
     char temp;
     temp = input[*iter+1];
     input[*iter+1] = input[*iter+2];
     input[*iter+2] = temp;
   }
-    if (input[1] != ' '){
-      char temp;
-      temp = input[1];
-      input[1] = input[0];
-      input[0] = temp;
-    }
+  // Swap first two letters for first word if it isn't a one-letter word
+  if (input[1] != ' '){
+    char temp;
+    temp = input[1];
+    input[1] = input[0];
+    input[0] = temp;
+  }
+  // Output
   cout << input;
   return 0;
 }
