db752fbece79b4324c3037611783d35be9d01f8e
U-BSD\412709 <412709@SHS-9YBF6X3.beaverton.k12.or.us>
2023-10-11 08:05:58 -0700

    working program

---
+++ swapFirstTwo.cpp
@@ -0,0 +1,39 @@
+/* This program swaps the first two letters of every word by finding the spaces in a string with a vector and an iterator
+ * Date: 10/11/23
+ * Author: Roger Li
+ */
+
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int main () {
+  vector<int> spaceIndexes;
+  char input[80];
+  cin.get(input, 81);
+  cin.get();
+  for (int i = 0; i < strlen(input) - 1; i++) {
+    if (input[i] == ' ' && input[i+2] != ' ') {
+      cout << "found a cool space!" << endl;
+      spaceIndexes.push_back(i);
+    }
+  }
+  for (vector<int>::iterator iter = spaceIndexes.begin(); iter != (spaceIndexes.end() - 1); iter++) {
+    cout << "here's a space" << endl;
+    char temp;
+    temp = input[*iter+1];
+    input[*iter+1] = input[*iter+2];
+    input[*iter+2] = temp;
+  }
+    if (input[1] != ' '){
+      char temp;
+      temp = input[1];
+      input[1] = input[0];
+      input[0] = temp;
+    }
+  cout << input;
+  return 0;
+}
